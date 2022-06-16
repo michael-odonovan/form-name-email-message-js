6fdb31e82a993ae1f41472a521b7097f04f4d724
michael o'donovan <mikeodonovan3@gmail.com>
2022-06-16 16:43:10 +0100

    feat: output data from form to div

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function myFunction() {
   event.preventDefault();
 
-  let nameData = document.querySelector('input[name="name"]').value
-  let emailData = document.querySelector('input[name="email"]').value
-  let messageData = document.querySelector('textarea').value
+  let nameData = document.querySelector('input[name="name"]').value;
+  let emailData = document.querySelector('input[name="email"]').value;
+  let messageData = document.querySelector('textarea').value;
+
+  document.getElementById('nameOutput').innerHTML = nameData;
+  document.getElementById('emailOutput').innerHTML = emailData;
+  document.getElementById('messageOutput').innerHTML = messageData
 
   console.log(messageData)
 }
